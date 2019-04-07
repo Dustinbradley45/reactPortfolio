f7a7ae40e90d0ec92386e41e8973ddd356ee1732
Dustin Bradley <dustinbradley@Dustins-MacBook-Air.local>
2019-04-06 20:10:39 -0400

    added raccoon svg

--- src/MainPage/LandingPage/LandingPage.js
+++ src/MainPage/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { TweenLite } from "gsap";
 import Hand from "./Hand.js";
+import Raccoon from "./Raccoon.js";
 
 class LandingPage extends Component {
     constructor() {
@@ -47,7 +48,7 @@ class LandingPage extends Component {
                    
                     
                     <h2 ref={h2=> this.headerTwo = h2}>Web Developer <span className="dev"> &#60; &#62;</span>.</h2>
-                    <h3 ref={h3 => this.headerThree = h3}>Toronto Lover.</h3>
+                    <h3 ref={h3 => this.headerThree = h3}>Toronto Lover <Raccoon />.</h3>
                 </div>
                     <div className="visually-hidden">Icons made by <a href="https://www.flaticon.com/authors/zlatko-najdenovski" title="Zlatko Najdenovski">Zlatko Najdenovski</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             </React.Fragment>
